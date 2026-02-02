06bc867f2cd0bbb125d8514a746356aa4c5134b7
Kajal Patel <kajal@example.com>
2026-02-02 16:24:05 +0530

    second commit

--- portfolio-website/src/pages/about/About.jsx
+++ portfolio-website/src/pages/about/About.jsx
@@ -1,5 +1,5 @@
 import Info from '../../components/Info';
-import Cv from '../../pdf/Kajal Resume.pdf';
+import Cv from '../../pdf/Kajal Freelancer.pdf';
 import { RiDownload2Line } from 'react-icons/ri';
 import Stats from '../../components/Stats';
 import { skill } from '../../Data';
